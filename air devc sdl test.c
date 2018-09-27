32d4dcc1ac01a305350deba31196987de9725946
xz2dai <40030489+xz2dai@users.noreply.github.com>
2018-09-27 23:54:02 +0800

    air环境测试
    对小米air12.5的编译环境测试

---
+++ air devc sdl test.c
@@ -0,0 +1,19 @@
+#include<SDL.h>
+#include<SDL_image.h>//IMG_Load函数所在头文件 
+#include<stdio.h>
+int main(int argc,char* argv[])
+{
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Surface* screen = NULL;//创建表面 
+	SDL_Surface* loadimage = NULL;
+	loadimage = IMG_Load("a.bmp");//载入图片 
+	screen = SDL_SetVideoMode(592,512,32,SDL_SWSURFACE);//设定窗口 
+	SDL_BlitSurface(loadimage,NULL,screen,NULL);//把lodeimage表面贴在screen上 
+	SDL_WM_SetCaption("My first SDL program",NULL);//窗口标签 
+	SDL_Flip(screen);//刷新 
+	SDL_Delay(2000);//延时 
+	SDL_FreeSurface(screen);
+	SDL_FreeSurface(loadimage);
+	SDL_Quit();
+	return 0;
+}
