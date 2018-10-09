46fb2ea46554ba4d582fdf24976e1e615bb8fd54
root <root@localhost>
2018-10-09 21:22:08 +0800

    sdl新建文件

--- SDL学习.c
+++ SDL学习.c
@@ -4,6 +4,13 @@
 int main(int argc,char* args[])
 {
 	SDL_Init(SDL_INIT_EVERYTHING);
-	Surface* screen
+	SDL_Surface* screen= NULL;
+	SDL_Surface* image= NULL;
+	if(image = SDL_LoadBMP("a.bmp") =
+	screen = SDL_SetVideoMode(592,512,32,SDL_SWSURFACE);
+	SDL_BlitSurface(image,NULL,screen,NULL);
+	SDL_Flip(screen);
+	SDL_Delay(2000);
+	SDL_FreeSurface(image);
 	SDL_Quit();
 }
