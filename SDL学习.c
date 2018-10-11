2e5c108bd3d56bcbbadd67b13f3ad4ed55329a6c
xz2dai <xz2dai@163.com>
2018-10-11 08:38:37 +0800

    链表实现

--- SDL学习.c
+++ SDL学习.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
 #include<SDL.h>
-#include<SDL_image.h>
 int main(int argc,char* args[])
 {
 	SDL_Init(SDL_INIT_EVERYTHING);
 	SDL_Surface* screen= NULL;
 	SDL_Surface* image= NULL;
-	if(image = SDL_LoadBMP("a.bmp") =
+	image = SDL_LoadBMP("a.bmp");
 	screen = SDL_SetVideoMode(592,512,32,SDL_SWSURFACE);
 	SDL_BlitSurface(image,NULL,screen,NULL);
 	SDL_Flip(screen);
