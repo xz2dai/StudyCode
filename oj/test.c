43f63a5ee7b466256c585f4d88348e0af6119961
xz2dai <40030489+xz2dai@users.noreply.github.com>
2018-10-17 00:08:37 +0800

    傻逼作业最后一题

--- oj/test.c
+++ oj/test.c
@@ -3,9 +3,14 @@
 #define PI 3.14159
 int main()
 {
-    double a,b, c,r;
-    scanf("%lf%lf%d", &b, &c, &a);
-    r = sqrt(b * b + c * c - 2 * b * c * cos((a/180)*PI));
-    printf("%.2lf", r);
+    double a,b,s, c,r;
+    scanf("%lf%lf%lf", &a,&b,&c);
+    if(a+b<=c || a+c<=b ||b+c<=a) printf("-1");
+    else 
+	{
+		s=(a+b+c)/2;
+		r = sqrt(s*(s-a)*(s-b)*(s-c));
+		printf("%lf",r);
+	}
     return 0;
 }
