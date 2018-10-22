271179081b2878723b13b0f7ea3c1845197d3a31
root <root@localhost>
2018-10-23 07:16:57 +0800

    gg

---
+++ SDL/touch test.c
@@ -0,0 +1,11 @@
+#include<SDL2/SDL.h>
+#include<stdio.h>
+#include<SDL2/SDL_ttf.h>
+#include<SDL2/SDL_image.h>
+int main(int argc,char* args[])
+{
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Surface* screen;
+	SDL_Surface* load;
+	SDL_WM_SetCaption("Touch Test");
+}
