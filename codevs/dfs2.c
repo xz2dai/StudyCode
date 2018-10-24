0db7487865c65a0442b66bfc6b715dd514ba109c
xz2dai <xz2dai@163.com>
2018-10-24 11:48:58 +0800

    鬼晓得哪里有毛病

--- codevs/dfs2.c
+++ codevs/dfs2.c
@@ -1,26 +1,27 @@
 #include<stdio.h>
-int a[10],book[10],totol;
+int a[10],book[10],totol=0;
 void dfs(int step);
 int main()
 {
 	dfs(1);
-	printf("Totol is %d",totol/2);
-	getchar();
+	printf("Total is %d",totol/2);
 	getchar();
 	return 0;
 }
 void dfs(int step)
 {
 	int i;
-	if (((a[1] * 100 + a[2] * 10 + a[3]) + (a[4] * 100 + a[5] * 10 + a[6])) == (a[7] * 100 + a[8] * 10 + a[9]))
-	{
-		totol++;
-		for (i = 1; i <= 9; i++) printf("%d\n", a[i]);
+	if(step==10)
+	{	if (a[1] * 100 + a[2] * 10 + a[3] + a[4] * 100 + a[5] * 10 + a[6] ==a[7] * 100 + a[8] * 10 + a[9] )
+		{
+			totol++;
+			printf("%d%d%d+%d%d%d=%d%d%d\n",a[1],a[2],a[3],a[4],a[5],a[6],a[7],a[8],a[9]);
+		}
 		return;
 	}
 	for(i=1;i<=9;i++)
 	{
-		if(book[i]!=0)
+		if(book[i]==0)
 		{
 			a[step] = i;
 			book[i] = 1;
