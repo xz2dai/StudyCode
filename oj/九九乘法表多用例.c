e94a962b58cee04c69f65a4b0fb55410c5f0c5a1
xz2dai <xz2dai@163.com>
2018-11-12 15:50:17 +0800

    mole

--- oj/九九乘法表多用例.c
+++ oj/九九乘法表多用例.c
@@ -1,18 +1,22 @@
 #include<stdio.h>
 int main()
 {
-	int k,j,l,m,n,s,s1,s2,i;
+	int k,j,l,m[101],n[101],s,s1,s2,i;
 	scanf("%d",&k);
 	for(i=1;i<=k;i++)
 	{
-		scanf("%d%d",&m,&n);
-		s=n-m;
+		scanf("%d%d",&m[i],n[i]);
+	}
+	for(i=1;i<=k;i++)
+	{
+		s=n[i]-m[i];
 		for(j=0;j<=s;j++)
 		{
 			for(s1=0;s1<j;s1++) printf("       ");
-			for(s2=m+j;s2<=n;s2++) printf("%d*%d=%-3d",m+j,s2,s2*(m+j));
-			if(i!=k) printf("\n");
+			for(s2=m[i]+j;s2<=n[i];s2++) printf("%d*%d=%-3d",m[i]+j,s2,s2*(m[i]+j));
+			printf("\n");
+			if(i!=k) printf("\n"); 
 		}
-		return 0;
 	}
+	return 0; 
 }
--- oj/方形盘龙 示例.c
+++ oj/方形盘龙 示例.c
@@ -1,38 +1,30 @@
 #include<stdio.h>
-int main() {
-	int N, i, j, k, n, m, count = 1, t;
-	scanf("%d", &t);
-	for (m = 0; m < t; m++) {
-		scanf("%d", &n);
-		int a[n][n];
-		if (n % 2 == 0) {
-			N = n / 2;
-		}
-		else {
-			N = n / 2 + 1;
-		}
-		for (i = 0; i < N; i++) {
-			for (j = i; j < n - i; j++) {
-				a[i][j] = count++;
-			}
-			k = n - i - 1;
-			for (j = i + 1; j < n - i; j++) {
-				a[j][k] = count++;
-			}
-			for (j = n - i - 2; j >= i; j--) {
-				a[k][j] = count++;
-			}
-			for (j = n - i - 2; j >= i + 1; j--) {
-				a[j][i] = count++;
-			}
-		}
-		for (i = 0; i < n; i++) {
-			for (j = 0; j < n; j++) {
-				printf("%4d", a[i][j]);
-			}
-			printf("\n");
-		}
-		printf("\n");
-	}
-	return 0;
+#include<math.h>
+int main()
+{
+   int n,i,j,k,cases,a[30][30];
+   scanf("%d",&cases);
+   while(cases--)
+   {
+       scanf("%d",&n);
+       for(i=0; i<n; i++)
+           for(j=0; j<n; j++)
+            a[i][j]=0;
+       i=j=-1;
+       k=1;
+       while(k<=n*n)
+       {
+           for(i++,j++; j<n&&a[i][j]==0; j++)a[i][j]=k++;
+           for(i++,j--; i<n&&a[i][j]==0; i++)a[i][j]=k++;
+           for(i--,j--; j>=0&&a[i][j]==0; j--)a[i][j]=k++;
+           for(i--,j++; i>=0&&a[i][j]==0; i--)a[i][j]=k++;
+       }
+       for(i=0; i<n; i++)
+       {
+           for(j=0; j<n; j++)printf("%4d",a[i][j]);
+           printf("\n");
+       }
+       printf("\n");
+   }
+   return 0;
 }
