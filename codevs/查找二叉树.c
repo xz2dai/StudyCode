2032b7f57a43845734269f110a457efb3ebb0acb
xz2dai <40030489+xz2dai@users.noreply.github.com>
2018-11-30 16:35:05 +0800

    查找二叉树

---
+++ codevs/查找二叉树.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct tree
+{
+	int self;
+	struct tree* lc;
+	struct tree* rc;
+};
+int main(int argc, char* args[])
+{
+
+}
--- oj/test.c
+++ oj/test.c
@@ -1,5 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
+
 int main()
 {
-    
+	char s1[1001], s2[1001];
+	gets(s1);
+	gets(s2);
+	if (strcmp(s1, s2) > 0) {
+		printf("s1比s2大");
+		return 0;
+	}
+	if (strcmp(s1, s2)==0) {
+		printf("s1与s2相同");
+		return 0;
+	}
+	if (strcmp(s1, s2)<0) {
+		printf("s1比s2小");
+		return 0;
+	}	
 }
