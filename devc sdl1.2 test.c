68e1c168ab97e92b4350c28a26f16720cd0bfe1b
root <root@localhost>
2018-12-04 07:05:25 +0800

    c4droid sdl问题解决

--- devc sdl1.2 test.c
+++ devc sdl1.2 test.c
@@ -1,15 +1,7 @@
-#include"SDL.h"
+#include "SDL/SDL.h"         //SDL的bug 加个空格就可以运行
 int main(int argc,char* args[])
 {
-	SDL_Surface* hello = NULL;
-	SDL_Surface* screen = NULL; 
 	SDL_Init(SDL_INIT_EVERYTHING);
-	screen = SDL_SetVideoMode(592,512,32,SDL_SWSURFACE);
-	hello = SDL_LoadBMP("a.bmp");
-	SDL_BlitSurface(hello,NULL,screen,NULL);
-	SDL_Flip(screen);
-	SDL_Delay(3000);
-	SDL_FreeSurface(hello);
 	SDL_Quit();
 	return 0;
 }
