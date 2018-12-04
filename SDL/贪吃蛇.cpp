a8061c2c2bf1d138e8c9f80d01af4dded883aed4
xz2dai <xz2dai@163.com>
2018-12-04 08:30:07 +0800

    贪吃蛇
    12.4贪吃蛇SDL2优化

--- SDL/贪吃蛇.cpp
+++ SDL/贪吃蛇.cpp
@@ -1,9 +1,9 @@
-#include<iostream>
-#include<SDL.h>
-#include<SDL_image.h>
-#include<SDL_ttf.h>
-#include<string>
-SDL_Surface *screen=NULL;
+#include <iostream>
+#include <SDL/SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <string>
+//SDL_Surface *screen=NULL;				12.4计划使用SDL2的createwindow
 SDL_Event event;
 class she                //蛇
 {
@@ -12,16 +12,13 @@ class she                //蛇
 		int y;
 		int vx;
 		int vy;
-		int length;
+		int length;			//蛇长度
 	public:
 		int move();
 		int grow();		   //吃了fruit则length++
-		int check();       //判断碰撞
+		int check();       //判断碰撞	12.4计划使用SDL2碰撞检测函数
 };
-int she::move();
-int she::grow();
-int she::check();
-class timer                  //计时器
+class timer                  //计时器    12.4计划更改为使用std::chrono模板 具体见cppreference
 {
 	private:
 		int starttime;
@@ -41,7 +38,7 @@ int init()
 	SDL_WM_SetCaption("贪吃蛇");
 	return 1;
 };
-SDL_Surface *loadimage(char* filename)
+SDL_Surface *loadimage(char* filename)			//12.4计划将surface转到render
 {
 	SDL_Surface *loadimage=NULL;
 	SDL_Surface *opimage=NULL;
