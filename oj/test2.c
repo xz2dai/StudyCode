5e6850a6ac20805504718b84573efd23aa6501da
xz2dai <40030489+xz2dai@users.noreply.github.com>
2018-12-07 18:00:58 +0800

    傻逼测验

--- oj/test2.c
+++ oj/test2.c
@@ -1,5 +1,24 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 int main()
 {
-    
+	int i, j,cup,k;
+	long long a[4];
+	for (i = 1; i <= 3;i++)
+		scanf("%lld", &a[i]);
+		for (i = 1; i <= 3; i++)
+		{
+			for (k = 1; k <= 3; k++)
+			{
+				if (a[k] > a[k + 1])
+				{
+					cup = a[k];
+					a[k] = a[k + 1];
+					a[k + 1] = cup;
+				}
+			}
+		}
+		printf("%lld->%lld->%lld", a[1], a[2], a[3]);
+		return 0;
 }
