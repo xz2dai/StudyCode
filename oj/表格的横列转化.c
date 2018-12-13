6ecdaac8487e7074f9d845827b88e2dee573028b
xz2dai <40030489+xz2dai@users.noreply.github.com>
2018-12-13 22:46:16 +0800

    睿智作业

---
+++ oj/表格的横列转化.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+void rollback(int m,int n)
+{
+    int map1[m+1][n+1],map2[n+1][m+1];
+    int i, j, k;
+    for (i = 1; i <= m;i++)
+    {
+        for (j = 1; j <= n;j++)
+        {
+            scanf("%d", &map1[i][j]);
+        }
+    }
+    for (i = 1; i <= n;i++)
+    {
+        for (j = 1; j <= m;j++)
+        {
+            printf("%d",map1[j][i]);
+            if(j!=m)
+                printf(" ");
+        }
+            printf("\n");
+    }
+
+}
+int main(int argc, char const *argv[])
+{
+    int n, i, a, b;
+    scanf("%d", &n);
+    for (i = 1; i <= n;i++)
+    {
+        scanf("%d%d", &a, &b);
+        rollback(a, b);
+        if(i!=n)
+            printf("\n");
+    }
+        return 0;
+}
