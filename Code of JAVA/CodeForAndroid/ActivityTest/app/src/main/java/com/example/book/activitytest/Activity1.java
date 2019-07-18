0bb1787bd0adbce3d57a6ae0ac22489fe392ef88
xz2dai <40030489+xz2dai@users.noreply.github.com>
2019-07-18 21:56:05 +0800

    update

--- Code of JAVA/CodeForAndroid/ActivityTest/app/src/main/java/com/example/book/activitytest/Activity1.java
+++ Code of JAVA/CodeForAndroid/ActivityTest/app/src/main/java/com/example/book/activitytest/Activity1.java
@@ -1,13 +1,35 @@
 package com.example.book.activitytest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
 
 public class Activity1 extends AppCompatActivity {
 
+
+    private Button button1,button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_1);
+
+        button1 = (Button)findViewById(R.id.button1);
+        button2 = (Button)findViewById(R.id.button2);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SwitchActivty();
+            }
+        });
+    }
+
+    private void SwitchActivty(){
+        Intent intent = new Intent(Activity1.this,Activity2.class);
+        startActivity(intent);
+
     }
 }
