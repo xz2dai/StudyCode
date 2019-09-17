19c7f85066729688c0142c827d0f0f24648569b3
xz2dai <40030489+xz2dai@users.noreply.github.com>
2019-09-17 15:59:47 +0800

    Revert "update"
    This reverts commit 107d184d0d0bb327ece34fc166849b8584d3a6d6.

--- Code of JAVA/java/src/Main.java
+++ Code of JAVA/java/src/Main.java
@@ -1,16 +1,9 @@
 import java.io.*;
-import java.util.Scanner;
 import javax.swing.JOptionPane;
 public class Main
 {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String str = scanner.nextLine();
-        if (str.equals("END")){
-            return;
-        } else {
-            System.out.println(str.toUpperCase());
-        }
+     String str = JOptionPane.showInputDialog("scanf:");
     }
 }
