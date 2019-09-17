107d184d0d0bb327ece34fc166849b8584d3a6d6
xz2dai <40030489+xz2dai@users.noreply.github.com>
2019-09-17 15:59:25 +0800

    update

--- Code of JAVA/java/src/Main.java
+++ Code of JAVA/java/src/Main.java
@@ -1,9 +1,16 @@
 import java.io.*;
+import java.util.Scanner;
 import javax.swing.JOptionPane;
 public class Main
 {
 
     public static void main(String[] args) {
-     String str = JOptionPane.showInputDialog("scanf:");
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        if (str.equals("END")){
+            return;
+        } else {
+            System.out.println(str.toUpperCase());
+        }
     }
 }
