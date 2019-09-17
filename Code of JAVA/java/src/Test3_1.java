f8fe2be3ee6a77d7b35d7157ca353e214e79a9ec
xz2dai <40030489+xz2dai@users.noreply.github.com>
2019-09-17 15:55:10 +0800

    On class code

---
+++ Code of JAVA/java/src/Test3_1.java
@@ -0,0 +1,12 @@
+public class Test3_1 {
+    public static void Question1Result(){
+        for(int i = 0 ; i <= 100 ;i+=5){
+            System.out.println("摄氏度："+i+" 华氏度："+ String.valueOf(GiveAnswer(i)));
+        }
+    }
+    public static double GiveAnswer(double cTemp){
+        double fTemp;
+        fTemp = cTemp*9/5+32;
+        return fTemp;
+    }
+}
