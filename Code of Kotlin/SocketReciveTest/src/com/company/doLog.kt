1afeeae5e7f0b4fe22ed97ec89e5c930231ceaa5
xz2dai <40030489+xz2dai@users.noreply.github.com>
2020-06-15 13:15:20 +0800

    update

---
+++ Code of Kotlin/SocketReciveTest/src/com/company/doLog.kt
@@ -0,0 +1,29 @@
+package com.company
+
+import java.io.File
+import java.util.*
+import javax.xml.crypto.Data
+
+/*
+一个用于写日志的类
+ */
+
+public class doLog {
+    val Logpath:String = "/www/TestServerLog/"
+    val file:File = File(Logpath)
+    init {
+
+    }
+    //添加日志
+    fun addLog(){
+
+    }
+    //新建日志
+    fun newLog(){
+
+    }
+    //删除日志
+    fun delLog(){
+
+    }
+}
