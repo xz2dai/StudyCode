b283432042b65bec90ffea97ad49b1927547938e
xz2dai <40030489+xz2dai@users.noreply.github.com>
2020-12-22 17:11:14 +0800

    updata

--- Code of JAVA/classProject/src/main/java/com/c611/classProject/web/servlet/LoginServlet.java
+++ Code of JAVA/classProject/src/main/java/com/c611/classProject/web/servlet/LoginServlet.java
@@ -2,6 +2,7 @@ package com.c611.classProject.web.servlet;
 
 import com.c611.classProject.bean.UserInfo;
 import com.c611.classProject.exception.UserInfoException;
+import com.c611.classProject.service.IUserService;
 import com.c611.classProject.service.impl.UserServiceImpl;
 
 import javax.servlet.ServletException;
@@ -26,42 +27,48 @@ public class LoginServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		/*
-		// 1.获取表单数据
-		// 获取用户名和密码
-		String account = request.getParameter("account");
-		String password = request.getParameter("password");
-		// 2.处理业务逻辑
-		IUserInfoService userInfoService = new UserInfoServiceImpl();
-		try {
-			UserInfo userInfo = userInfoService.login(account, password);
-			// userInfo == null：用户名和密码错误
-			if (userInfo == null) {
-				request.setAttribute("login_error", "用户名或者密码错误...");
-				// 请求转发到登录页面
-				request.getRequestDispatcher("/index.jsp").forward(request, response);
-			} else {
-				// 登录成功，重定向到首页
-				response.sendRedirect(request.getContextPath() + "/system/index/index.html");
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			// 出现异常
-			request.setAttribute("login_error", e.getMessage());
-			// 请求转发到登录页面
-			request.getRequestDispatcher("/index.jsp").forward(request, response);
-		}
-		// 通过用户名和密码进行登录
-		// 3.分发转向
-		*/
+
+//		// 1.获取表单数据
+//		// 获取用户名和密码
+//		String account = request.getParameter("account");
+//		String password = request.getParameter("password");
+//		// 2.处理业务逻辑
+//		IUserService userInfoService = new UserServiceImpl();
+//		try {
+//			UserInfo userInfo = userInfoService.f(account, password);
+//			// userInfo == null：用户名和密码错误
+//			if (userInfo == null) {
+//				request.setAttribute("login_error", "用户名或者密码错误...");
+//				// 请求转发到登录页面
+//				request.getRequestDispatcher("/index.jsp").forward(request, response);
+//			} else {
+//				// 登录成功，重定向到首页
+//				response.sendRedirect(request.getContextPath() + "/system/index/index.html");
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//			// 出现异常
+//			request.setAttribute("login_error", e.getMessage());
+//			// 请求转发到登录页面
+//			request.getRequestDispatcher("/index.jsp").forward(request, response);
+//		}
+//		// 通过用户名和密码进行登录
+//		// 3.分发转向
+
 		UserInfo userInfo = null;
 		try {
 			userInfo= new UserServiceImpl().getUserInfoById("3");
 		} catch (UserInfoException e) {
 			e.printStackTrace();
 		}
-		request.getSession().setAttribute("user_info",userInfo);
-		response.sendRedirect(request.getContextPath()+"/content/makeorder.jsp");
+		if(userInfo == null){
+			request.setAttribute("error_info","未查找到此用户！");
+			request.getRequestDispatcher("/index.jsp").forward(request,response);
+		}else{
+			request.getSession().setAttribute("user_info",userInfo);
+			response.sendRedirect(request.getContextPath()+"/content/makeorder.jsp");
+		}
+
 	}
 
 	@Override
